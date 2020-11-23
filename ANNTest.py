850f3b444b260192db37ec2d453a2b150b27183d
maxp7 <maximilian.pickl@gmx.de>
2020-11-23 09:23:33 +0100

    Changes to ANN; Depot req. actualized

--- ANNTest.py
+++ ANNTest.py
@@ -1,22 +1,26 @@
 import pandas as pd
-import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense
+import numpy
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+
 #import the data
 node_0 = pd.read_csv(r"Prepared Data/Data_Prepared_node0.csv",sep=";")
 
-x_train = node_0.drop("Running App ID",axis=1)
+x_train = node_0.drop(["Running App ID", "Running App"],axis=1)
 y_train = node_0.loc[:,"Running App ID"]
 
-y_train = y_train.astype('category')
+x_train = x_train.to_numpy()
+y_train = to_categorical(y_train)
 
 # defining model    
 print('Defining Model')
 
 model = Sequential()
 
-model.add(Dense(12, activation="relu", input_shape=(len(x_train[0]),)))
-model.add(Dense(120, activation="sigmoid"))
+model.add(Dense(12, input_shape=(len(x_train[0]),)))
+model.add(Dense(120, activation="tanh"))
 model.add(Dense(49, activation="relu"))
 model.add(Dense(7, activation="softmax"))
 
@@ -24,7 +28,7 @@ model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=["accura
 
 print('Training Data Node0')
 model.fit(
-    x_train.reshape(len(x_train), len(x_train[0])),
+    x_train,
     y_train,
     epochs=10,
-    batch_size=5)
+    batch_size=5,validation_split=0.3)
